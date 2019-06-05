afc2ab308bc8bc659dd6757e9a8dd97c01c15101
Itay Azulay <itay.responder@gmail.com>
2019-06-05 17:58:39 +0300

    share-fb

---
+++ js/main-util.js
@@ -0,0 +1,8 @@
+// facebook api
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/he_IL/sdk.js#xfbml=1&version=v3.0&appId=807866106076694&autoLogAppEvents=1';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
