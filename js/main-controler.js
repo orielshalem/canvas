82de6db43f31b93c21cd25a2af54bb9798ab19bd
Itay Azulay <itay.responder@gmail.com>
2019-06-05 16:46:29 +0300

    on init

--- js/main-controler.js
+++ js/main-controler.js
@@ -1,8 +1,14 @@
 'use strict';
 
-const canvas = document.getElementById('canvas');
-console.log(canvas)
-const ctx = canvas.getContext('2d');
+let canvas;
+let ctx;
+
+function onInit() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth - 50
+    canvas.height = window.innerHeight - 100
+}
 
 function draw(ev) {
     ctx.save()
@@ -30,23 +36,25 @@ function getColor() {
     return document.querySelector('.color').value;
 }
 
-function drawRect(x,y) {
-    ctx.rect(x,y, 50, 50)
-    ctx.fillStyle = getColor()
-    ctx.fillRect(x,y, 50, 50)
+function drawRect(x, y) {
+    ctx.rect(x, y, 50, 50)
+    ctx.strokeStyle = getColor()
+    ctx.strokeRect(x, y, 50, 50)
 }
 
 function drawArc(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, 50, 0, 2 * Math.PI);
+    ctx.strokeStyle = getColor();
     ctx.stroke();
 }
 
 function drawTriangle(x, y) {
     ctx.beginPath();
-    ctx.moveTo(y, y-30);
+    ctx.moveTo(y, y - 30);
     ctx.lineTo(x, y);
-    ctx.lineTo(x, x-5);
+    ctx.lineTo(x, x - 5);
     ctx.closePath()
+    ctx.strokeStyle = getColor();
     ctx.stroke();
 }
