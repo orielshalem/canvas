b6d91211f8d79732c7f2f10416cb667d968b1a6b
Oriel Shalem <orielshalem@gmail.com>
2019-06-05 15:41:54 +0300

    rect

--- js/main-controler.js
+++ js/main-controler.js
@@ -4,5 +4,35 @@ const canvas = document.getElementById('canvas');
 console.log(canvas)
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+
+let gCurrElement = 'rect'
+
+
+function draw(ev) {
+    ctx.save()
+    console.log(ev)
+    const {offsetX, offsetY} = ev
+
+    switch (gCurrElement) {
+        case 'triangle':
+            drawTriangle()
+            break;
+        case 'rect':
+            drawRect(offsetX, offsetY)
+            break;
+        case 'text':
+            drawText('test',offsetX, offsetY)
+            break;
+    }
+    ctx.restore()
+}
+
+
+
+function drawRect(x,y) {
+    ctx.rect(x,y, 150, 200)
+    ctx.fillStyle = 'white'
+    ctx.fillRect(x,y, 150, 150)
+    ctx.stroke()
+    // ctx.fill()
+}
